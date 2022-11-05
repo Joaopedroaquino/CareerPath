615f8da710799eeeed78394574ee054a085e3758
Joao Pedro Aquino <aqerhack@gmail.com>
2022-11-05 10:32:30 -0300

    config appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5437,
+      username: 'root',
+      password: 'root',
+      database: 'root',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    }
+
+
+  ),],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
