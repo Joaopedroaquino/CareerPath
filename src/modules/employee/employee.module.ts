5c5bcf6ae92784fd39b7f045484cd92d9edb4689
joaopedroaquino <aqerhack@gmail.com>
2022-11-08 14:58:32 -0300

    criado a rota get/swagger

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './contracts/entities/Employee.entity';
+import { EmployeeRouter } from './routes/employee.routes';
 
 @Module({
     imports:[ TypeOrmModule.forFeature([Employee])],
+    controllers:[EmployeeRouter]
 
 })
 export class EmployeeModule {}
--- src/modules/employee/routes/employee.routes.ts
+++ src/modules/employee/routes/employee.routes.ts
@@ -1,9 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { IReturnEmployeeDTO } from "../contracts/dto/IReturnEmployeeDTO";
 
 @ApiTags('Employee')
 @Controller('employee')
 export class EmployeeRouter{
     constructor(){}
+
+    @ApiResponse({type: IReturnEmployeeDTO})
+    @Get()
+    public async getAll(){
+        return {}
+    }
+
    
 }
